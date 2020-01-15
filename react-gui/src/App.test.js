c40b144b7d863471ce118444340fec237100dab2
Mohamed <mamdouni@salto-consulting.com>
2020-01-15 18:10:12 +0100

    [fix] MAI - fix of the react test issue

--- react-gui/src/App.test.js
+++ react-gui/src/App.test.js
@@ -1,3 +1,8 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-test("renders learn react link", () => {});
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+});
